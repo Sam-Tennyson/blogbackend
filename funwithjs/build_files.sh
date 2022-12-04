f55ed8f238329efb3f7fb4a34241f1fa2d99ee65
Saurabh Shukla <84230669+Sam-Tennyson@users.noreply.github.com>
2022-12-04 19:18:14 +0530

    changepython-version

--- funwithjs/build_files.sh
+++ funwithjs/build_files.sh
@@ -1,4 +1,4 @@
 echo "BUILD START"
-python 3.9 -m pip install -r requirements.txt
-python 3.9 manage.py collectstatic --no-input --clear
+python 3.10 -m pip install -r requirements.txt
+python 3.10 manage.py collectstatic --no-input --clear
 echo "BUILD END"
